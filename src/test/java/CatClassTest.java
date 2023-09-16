dbca85302d47127029cdd2ff22f1b066f9abc242
Ledovskikh Viktoria <vl223@bk.ru>
2023-09-16 23:53:01 +0300

    удалила старый класс тестов CatTests.java добавила новый CatClassTest.java для тестирования класса Cat

--- src/test/java/CatClassTest.java
+++ src/test/java/CatClassTest.java
@@ -5,43 +5,45 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
-
 import java.util.Arrays;
+import java.util.List;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class CatTests {
+public class CatClassTest {
 
     @Mock
     Feline feline = new Feline();
 
 
-    Cat cat = new Cat(feline);
-
     @Test
-    public void catGetSoundReturnMya(){
+    public void catGetSoundReturnMya() {
+        Cat cat = new Cat(feline);
         Assert.assertEquals("Вернулось не верное значение, ожидалось Мяу!", "Мяу", cat.getSound());
     }
 
     @Test
     public void catGetFoodVerifyFeline() throws Exception {
         Cat cat = new Cat(feline);
-    when(feline.eatMeat()).thenReturn(Arrays.asList("Кар, Кар, Кар"));
-    cat.getFood();
-    verify(feline, Mockito.times(1)).eatMeat();
-
-
-
-
-
+        when(feline.eatMeat()).thenReturn(Arrays.asList("Кар, Кар, Кар"));
+        cat.getFood();
+        verify(feline, Mockito.times(1)).eatMeat();
 
     }
 
-
-
-
+    @Test
+    public void catGetFoodReturnText() throws Exception {
+        Cat cat = new Cat(feline);
+        List<String> predator = List.of("Животные", "Птицы", "Рыба");
+        when(cat.getFood()).thenReturn(predator);
+        Assert.assertEquals("Ошибка!! Тест не возвращает значение "+ predator, predator, cat.getFood());
+    }
 }
+
+
+
+
+
